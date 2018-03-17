3e90014536ea283accb8b423fd9a7005a7de6d8a
Tim Groeneboom <tim@timgroeneboom.com>
2018-03-17 12:20:53 +0100

    initial commit

---
+++ Assets/Scripts/CodeFlow/Message.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace CodeFlow
+{
+    internal abstract class MessageBroadCasterBase { }
+
+    internal class MessageBroadCaster<T> : MessageBroadCasterBase where T : Message
+    {
+        protected List<Action<T>> _callbacks = new List<Action<T>>();
+        protected List<Action<T>> _callbacksToRemove = new List<Action<T>>();
+        protected bool _broadCasting = false;
+
+        public void AddListener(Action<T> callback) 
+        {
+            if (!_callbacks.Contains(callback))
+            {
+                _callbacks.Add(callback);
+            }
+            else
+            {
+                Debug.LogError("MessageBroadCaster<" + typeof(T).ToString() + "> -> already contains this callback");
+            }
+        }
+
+        public void RemoveListener(Action<T> callback)
+        {
+            if( _broadCasting )
+            {
+                _callbacksToRemove.Add(callback);
+            }else
+            {
+                _callbacks.Remove(callback);
+            }
+        }
+
+        public void BroadCast(T message)
+        {
+            _broadCasting = true;
+            foreach(var callback in _callbacks)
+            {
+                if( callback != null )
+                {
+                    callback.Invoke(message);
+                }else
+                {
+                    Debug.LogError("MessageBroadCaster<" + typeof(T).ToString() + "> -> callback == null");
+                }  
+            }
+            _broadCasting = false;
+
+            foreach (var callback in _callbacksToRemove)
+            {
+                _callbacks.Remove(callback);
+            }
+            _callbacksToRemove.Clear();
+        }
+    }
+
+    public abstract class Message
+    {
+        static internal Dictionary<Type, MessageBroadCasterBase> s_instances = new Dictionary<Type, MessageBroadCasterBase>();
+
+        static public void AddListener<T>(Action<T> callback) where T : Message
+        {
+            GetBroadcasterInstance<T>().AddListener(callback);
+        }
+
+        static public void RemoveListener<T>(Action<T> callback) where T : Message
+        {
+            GetBroadcasterInstance<T>().RemoveListener(callback);
+        }
+
+        static public void BroadCast<T>(T message) where T : Message
+        {
+            GetBroadcasterInstance<T>().BroadCast(message);
+        }
+
+        static private MessageBroadCaster<T> GetBroadcasterInstance<T>() where T : Message
+        {
+            var type = typeof(T);
+            if (!s_instances.ContainsKey(type))
+            {
+                s_instances.Add(type, new MessageBroadCaster<T>());
+            }
+
+            return s_instances[type] as MessageBroadCaster<T>;
+        }
+    }
+}
---
+++ Assets/Scripts/MessageTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeFlow
+{
+    public class CustomMessage : Message
+    {
+        public string HelloWorldString = "Hello World";
+    }
+
+    public class OtherCustomMessage : Message
+    {
+        public int MyCustomInteger = -1;
+    }
+
+    public class MessageExample : MonoBehaviour
+    {
+        void Start()
+        {
+            Message.AddListener<CustomMessage>(OnCustomMessage);
+            Message.AddListener<OtherCustomMessage>(OnOtherCustomMessage);
+
+            var customMessage = new CustomMessage();
+            Message.BroadCast(customMessage); // OnCustomMessage should be called
+            customMessage.HelloWorldString = "Should not be received";
+            Message.BroadCast(customMessage); // OnCustomMessage should not be called
+            Message.BroadCast(new OtherCustomMessage()); // OnOtherCustomMessage should be called
+            Message.AddListener<OtherCustomMessage>(OnOtherCustomMessage); // error should be thrown
+            Message.BroadCast(new OtherCustomMessage 
+            {
+                MyCustomInteger = 666
+            }); // OnOtherCustomMessage should be called
+
+            customMessage.HelloWorldString = "Should be received";
+            Action<CustomMessage> callbackAction = null;
+            callbackAction = new Action<CustomMessage>(message =>
+           {
+               Debug.Log(message.HelloWorldString);
+               Message.RemoveListener(callbackAction);
+           });
+            Message.AddListener<CustomMessage>(OnCustomMessage);
+            Message.BroadCast(customMessage); // callbackAction should be called
+            Message.RemoveListener(callbackAction);
+            Message.BroadCast(customMessage); // callbackAction should not be called
+        }
+
+        private void OnOtherCustomMessage(OtherCustomMessage message)
+        {
+            Debug.Log("my custom integer : " + message.MyCustomInteger);
+        }
+
+        private void OnCustomMessage(CustomMessage message)
+        {
+            Debug.Log(message.HelloWorldString);
+            Message.RemoveListener<CustomMessage>(OnCustomMessage);
+        }
+    }
+}
